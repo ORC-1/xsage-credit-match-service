2b0ebe4e0c04cbdd9799ae5a360f15960b34dbab
Chimaobi <chimaobi@kibrismikro.com>
2025-01-31 17:18:42 +0100

    incipit

---
+++ src/test/java/com/xsage/xsagecreditmatchservice/XsageCreditMatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsage.xsagecreditmatchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsageCreditMatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
